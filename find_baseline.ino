0ca80ac0b327d6daa28270219770be477791febb
Emily Mallaber <37315680+emmalla@users.noreply.github.com>
2019-04-04 16:23:58 -0400

    Add files via upload
    added comments so I can understand myself in a week

--- find_baseline.ino
+++ find_baseline.ino
@@ -1,11 +1,13 @@
-long[] restVals = [0,0,0,0,0,0,0,0,0,0];
-long[] flexVals  [0,0,0,0,0,0,0,0,0,0];
-boolean[] go = true;
-long sensorValue;
-long restAvg = 0;
-long restMax;
-long flexAvg = 0;
-long flexMin;
+
+
+long[] restVals = [0,0,0,0,0,0,0,0,0,0]; // 10 trials of rest data 
+long[] flexVals  [0,0,0,0,0,0,0,0,0,0]; // 10 trials of flex data
+boolean[] go = true; // outer control condition
+long sensorValue; // reads from sensor
+long restAvg = 0; // holds avg of rest vals
+long restMax; // highest rest val to find safe threshold
+long flexAvg = 0; // holds avg of flex vals
+long flexMin; // lowest flex val to find safe threshold
 
 
 void setup() {
@@ -14,7 +16,10 @@ void setup() {
 
 }
 
-void loop() {
+void loop() { 
+  
+ // gathers 10 data points from each rest and flexing
+  
   while(go = true){
     Serial.println("Relax");
     delay(1000);
@@ -25,12 +30,13 @@ void loop() {
       Serial.print(i);
       Serial.print(" :");      
       Serial.println(sensorValue);      
-      delay(100o);
+      delay(1000);
      }
-    Serial.println();
-    Serial.println("Flex");
-    delay(1000);
-    for(int j = 0; j < 10; j++){
+     delay(5000);
+     Serial.println();
+     Serial.println("Flex");
+     delay(1000);
+     for(int j = 0; j < 10; j++){
       sensorValue = analogRead(A0); 
       flexVals[j] = sensorValue;
       Serial.print("Flex Value ");
@@ -49,8 +55,12 @@ void loop() {
       restMax = restMax + restVals[i];
     }
     restAvg = restAvg/10;
-    go = false;
+    go = false; // end after one iteration, wait for reset
   }
+
+  // print out the averages, min, and max
+  // add code that can determine and output threshold after preliminary experimentation
+  
   Serial.println();
   Serial.printnl();
   Serial.print("Resting Avg: ");
