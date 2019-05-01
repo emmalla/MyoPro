fd7a89eaa221d23107fa3964f96bb7832dc3a389
Emily Mallaber <37315680+emmalla@users.noreply.github.com>
2019-05-01 14:10:36 -0400

    HandCode.ino

--- HandCode.ino
+++ HandCode.ino
@@ -1,31 +1,38 @@
-
 #include <Servo.h>
 
-int motor = 7; 
+int motor = 7;
+int motor2 = 4; 
 int sensorVal;
-byte state = 0; // 0 means hand is open, 1 means hand is closed
-Servo hand;
+byte state = 0;
+Servo thumb;
+Servo fingers;
 byte lastState = 0;
 int pos = 0;
 
 
 void setup() {
+  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(A0, INPUT);
-  hand.attach(motor);
-  hand.write(0);
+  thumb.attach(motor);
+  thumb.write(0);
+  fingers.attach(motor2);
+  fingers.write(0);
 }
 
 void loop() {
-  sensorVal = analogRead(A0); 
+  // put your main code here, to run repeatedly:
+
+  
+  sensorVal = analogRead(A0);
   Serial.println(sensorVal);
-  while(sensorVal > 500){
-    pos = 180-(180*state);
-    hand.write(pos);
-    delay(2000);   
-    state = 1-state;  
+  while(sensorVal > 250 && sensorVal < 600) {
+    pos = 180 - (180*state);
+    thumb.write(pos);
+    fingers.write(pos);
+    delay(2000);
+    state = 1-state;
     sensorVal = 0;
     delay(100);
- }
-
+  }
 }
